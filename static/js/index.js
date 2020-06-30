758b79209baad20fee467d1b34859c1c8b0524f6
hugolpz <hugo.lpz@gmail.com>
2020-06-30 21:53:28 +0200

    Change sidebar width to add info in parcelles.

--- static/js/index.js
+++ static/js/index.js
@@ -203,8 +203,8 @@ function idSectionToCode(idSection) {
 
 /* Set the width of the sidebar to 250px and the left margin of the page content to 250px */
 function openNav() {
-	document.getElementById("mySidebar").style.width = "250px";
-	document.getElementById("main").style.marginLeft = "250px";
+	document.getElementById("mySidebar").style.width = "350px";
+	document.getElementById("main").style.marginLeft = "350px";
 }
 
 /* Set the width of the sidebar to 0 and the left margin of the page content to 0 */
